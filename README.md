d0ac33d6a26b0302d6a8db38bd30272b812da079
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-10-25 12:56:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-rx5zoz
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/kalburgimanjunath/stackblitz-starters-rx5zoz)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import { Header } from './components/index';
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
